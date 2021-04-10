1d1a90a29e9556b64a8edc41d847ab02442aee44
Heiko Wagner <heikowagner@t-online.de>
2021-04-10 10:53:33 +0200

    update versions

--- dasky.py
+++ dasky.py
@@ -7,80 +7,12 @@ import time
 import logging
 import subprocess
 import re
+from  kubernetes_helper import start_dask_cluster, delete_dask_cluster
 
 # We determine the ip of the master node using kubectl
 p = subprocess.Popen("kubectl cluster-info", stdout=subprocess.PIPE)
 kube_conf = p.stdout.read().decode()
 master_ip = re.findall(r"//([\s\S]*?):", kube_conf, re.MULTILINE)[0]
-
-config.load_kube_config()
-k8s_apps_v1 = client.AppsV1Api()
-k8s_core_v1 = client.CoreV1Api()
-
-
-def start_dask_cluster(
-    namespace="default", worker_replicas=5, pip_packages=None, apt_packages=None
-):
-    with open(path.dirname(__file__) + "/service.yaml") as f:
-        dep = yaml.safe_load(f)
-        try:
-            k8s_core_v1.delete_namespaced_service("master-node", namespace=namespace)
-        except: # noqa
-            pass
-        resp = k8s_core_v1.create_namespaced_service(body=dep, namespace=namespace)
-        print(resp)
-
-    with open(path.dirname(__file__) + "/sheduler.yaml") as f:
-        resp = update_or_deploy(f, pip_packages=pip_packages, apt_packages=apt_packages)
-    with open(path.dirname(__file__) + "/worker.yaml") as f:
-        resp = update_or_deploy(
-            f,
-            replicas=worker_replicas,
-            pip_packages=pip_packages,
-            apt_packages=apt_packages,
-        )
-
-
-def delete_dask_cluster(namespace):
-    k8s_core_v1.delete_namespaced_service("master-node", namespace=namespace)
-    k8s_apps_v1.delete_namespaced_deployment("sheduler", namespace=namespace)
-    k8s_apps_v1.delete_namespaced_deployment("worker", namespace=namespace)
-    logging.info("dask cluster deleted")
-
-
-def update_or_deploy(
-    file, namespace="default", replicas=1, pip_packages=None, apt_packages=None
-):
-    dep = yaml.safe_load(file)
-    logging.debug(dep)
-    dep["spec"]["replicas"] = replicas
-    # put packages here
-    name = dep["metadata"]["name"]
-    try:
-        resp = k8s_apps_v1.read_namespaced_deployment(name, namespace=namespace)
-        logging.info("Deployment already present. status='%s'" % resp.metadata.name)
-        resp = k8s_apps_v1.replace_namespaced_deployment(
-            name=name, namespace=namespace, body=dep
-        )
-        logging.info("Deployment updated. status='%s'" % resp.metadata.name)
-    except client.exceptions.ApiException as e:
-        if e.reason == "Not Found":
-            resp = k8s_apps_v1.create_namespaced_deployment(
-                body=dep, namespace=namespace
-            )
-            logging.info("Deployment created. status='%s'" % resp.metadata.name)
-        else:
-            raise client.exceptions.ApiException(e)
-
-    while True:
-        resp = k8s_apps_v1.read_namespaced_deployment(name=name, namespace="default")
-        if resp.status.ready_replicas == replicas:
-            break
-        time.sleep(1)
-        logging.info("pending...")
-    logging.info("Done.")
-    return resp
-
 
 def main():
     start_dask_cluster()
@@ -92,7 +24,6 @@ def main():
 
     print(z.compute())
     delete_dask_cluster(namespace="default")
-
 
 if __name__ == "__main__":
     main()
