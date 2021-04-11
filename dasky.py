3e64d810876dcdf078243e11d1d274c5563b4536
Heiko Wagner <heikowagner@t-online.de>
2021-04-11 12:06:21 +0200

    Update dasky.py

--- dasky.py
+++ dasky.py
@@ -11,14 +11,16 @@ master_ip = re.findall(r"//([\s\S]*?):", kube_conf, re.MULTILINE)[0]
 
 
 def main():
-    start_dask_cluster()
-    dask_client = Client(master_ip + ":30086") # noqa
+    start_dask_cluster(namespace="default")
 
+    dask_client = Client(master_ip + ":30086") # noqa
+    
+    # Run the computation at the cluster
     x = da.random.random((10000, 10000), chunks=(1000, 1000))
     y = x + x.T
     z = y[::2, 5000:].mean(axis=1)
-
     print(z.compute())
+
     delete_dask_cluster(namespace="default")
 
 
